064e99ffc11901099ea22a34b08b53eea693fc27
francesco valenti <FRANCESCOVALENTI83@OUTLOOK.IT>
2025-08-05 12:29:24 +0200

    feat : ultima correzzione su error

--- src/main/java/org/platform/ticket/ticket_platform/controller/CustomErrorController.java
+++ src/main/java/org/platform/ticket/ticket_platform/controller/CustomErrorController.java
@@ -2,7 +2,7 @@ package org.platform.ticket.ticket_platform.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,26 +10,23 @@ import jakarta.servlet.http.HttpServletResponse;
 @Controller
 public class CustomErrorController implements ErrorController {
 
-     // …
+    @RequestMapping("/error")
+    public ModelAndView handleError(HttpServletRequest request, HttpServletResponse response) {
+        ModelAndView modelAndView = new ModelAndView();
 
-     @RequestMapping("/error")
-     public ModelAndView handleError(HttpServletResponse response) {
-          ModelAndView modelAndView = new ModelAndView();
+        int status = response.getStatus();
 
-          if (response.getStatus() == HttpStatus.NOT_FOUND.value()) {
-               modelAndView.setViewName("error-404");
-          } else if (response.getStatus() == HttpStatus.FORBIDDEN.value()) {
-               modelAndView.setViewName("error-403");
-          } else if (response.getStatus() == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-               modelAndView.setViewName("error-500");
-          } else {
-               modelAndView.setViewName("error");
-          }
-          
+        if (status == HttpStatus.NOT_FOUND.value()) {
+            modelAndView.setViewName("error/error-404");
+        } else if (status == HttpStatus.FORBIDDEN.value()) {
+            modelAndView.setViewName("error/error-403");
+        } else if (status == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+            modelAndView.setViewName("error/error-500");
+        } else {
+            modelAndView.setViewName("error/error"); 
+        }
 
-          modelAndView.addObject("status",response.getStatus());
-          return modelAndView;
-     }
-
- 
+        modelAndView.addObject("status", status);
+        return modelAndView;
+    }
 }
